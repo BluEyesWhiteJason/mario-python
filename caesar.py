cd9bbe6f72d2a021338e4178b1f5fbe8433c7722
BluEyesWhiteJason <jason.speet@gmail.com>
2019-07-08 18:30:32 -0400

    Did most of it, not quite working yet

--- caesar.py
+++ caesar.py
@@ -1,3 +1,21 @@
 import sys
 
-if sys.argc != 2 
+if len(sys.argv) != 2:
+    print("Wrong number of arguments")
+
+#Convert CLA to int
+num = int(sys.argv[1])
+
+#Get string to be encoded
+word = input("Enter string: ")
+
+
+#loop to change nums
+for i in range int(len(word)):
+    newWord[i] = chr(ord(word[i]) + num)
+
+#Print result
+print(newWord)
+
+
+
