7f1465d5d3f3e9ef40f875165f2260218f2b9324
BluEyesWhiteJason <jason.speet@gmail.com>
2019-07-23 13:33:33 -0400

    Try catch working properly

--- Lab8Objects/Lab8Objects/Program.cs
+++ Lab8Objects/Lab8Objects/Program.cs
@@ -16,42 +16,45 @@ namespace Lab8Objects
             Student s5 = new Student("Maricela", "Morelia", "Tacos");
 
             //place into list
-            List<Student> students = new List<Student>();
-            students.Add(s1);
-            students.Add(s2);
-            students.Add(s3);
-            students.Add(s4);
-            students.Add(s5);
+            List<Student> students = new List<Student>
+            {
+                s1,
+                s2,
+                s3,
+                s4,
+                s5
+            };
 
             // Get input on which student, catch exceptions
             Console.WriteLine("Welcome to our C# class.");
 
             do
             {
-                Console.WriteLine("Which student would you like to learn more about ? (enter a number 1 - 10):");
+                Console.WriteLine("Which student would you like to learn more about ? (enter a number 1 - {0}):", students.Count);
                 for (int i = 0; i < students.Count; i++)
                 {
                     Console.WriteLine((i + 1) + ". " + students[i].GetName());
                 }
 
                 int input = 0;
-                try
+                while (true)
                 {
-                    input = int.Parse(Console.ReadLine());
-                }
-                catch (FormatException e)
-                {
-                    Console.WriteLine("Please input a number");
-                }
 
-                try
-                {
-                    Console.WriteLine("Student {0} is {1}. What would you like to know about them?", input, students[input - 1].GetName());
-                    Console.WriteLine("Try 'hometown' for hometown or 'food' for favorite food");
-                }
-                catch (ArgumentOutOfRangeException e)
-                {
-                    Console.WriteLine("Please enter a number from 1-5");
+                    try
+                    {
+                        input = int.Parse(Console.ReadLine());
+                        Console.WriteLine("What about {0} would you like to know?", students[input - 1].GetName());
+                        Console.WriteLine("Try 'hometown' for hometown or 'food' for favorite food");
+                        break;
+                    }
+                    catch (FormatException e)
+                    {
+                        Console.WriteLine("Invalid input, please enter a number");
+                    }
+                    catch (ArgumentOutOfRangeException e)
+                    {
+                        Console.WriteLine("Invalid input, please enter a number from 1-{0}", students.Count);
+                    }
                 }
 
                 // get what info user wants
@@ -86,7 +89,7 @@ namespace Lab8Objects
 
         public static bool Proceed()
         {
-            Console.WriteLine("Continue? y/n : ");
+            Console.WriteLine("Learn about a different student? y/n : ");
             string contEnter = Console.ReadLine();
 
             // Check if they want to continue
